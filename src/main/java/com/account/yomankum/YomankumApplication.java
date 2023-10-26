e1c6d968e6f4f9a75b14a12feb3a74d243a3a18e
h0l1da2 <whaleee@naver.com>
2023-10-26 19:13:46 +0900

    first commit

---
+++ src/main/java/com/account/yomankum/YomankumApplication.java
@@ -0,0 +1,13 @@
+package com.account.yomankum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomankumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YomankumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/account/yomankum/YomankumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.yomankum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YomankumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
