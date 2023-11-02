01e5681aec29bb45aab7c7dea82008f54fb9d34b
h0l1da2 <whaleee@naver.com>
2023-11-02 16:05:19 +0900

    fix: value 참조 추가
    application.yml 참조할 수 있도록 명시 수정

--- src/main/java/com/account/yomankum/config/jwt/TokenParser.java
+++ src/main/java/com/account/yomankum/config/jwt/TokenParser.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -12,6 +13,7 @@ import java.security.Key;
 import java.util.Date;
 
 @Component
+@PropertySource("classpath:application.yml")
 public class TokenParser {
     private final JwtParser jwtParser;
     public TokenParser(@Value("${token.secret.key}") String secretKey) {
