e2b517be7b661a347d97cb003f7005dccfab589b
h0l1da2 <whaleee@naver.com>
2023-11-02 12:27:10 +0900

    refactor: 필요없는 애노테이션 삭제
    애노테이션 삭제

--- src/test/java/com/account/yomankum/service/impl/SignUpServiceImplTest.java
+++ src/test/java/com/account/yomankum/service/impl/SignUpServiceImplTest.java
@@ -11,14 +11,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
-@ActiveProfiles("test")
 @SpringBootTest
 class SignUpServiceImplTest {
 
