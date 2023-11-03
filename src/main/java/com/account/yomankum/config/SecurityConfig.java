0c44bcf4ec690a2c35794ea6d4b38cf7c4f8a510
h0l1da2 <whaleee@naver.com>
2023-11-03 12:26:22 +0900

    feat: Security 설정 추가
    필요 없는 부분 주석
    로그아웃하면 로그인 페이지로 갈 수 있도록 설정

--- src/main/java/com/account/yomankum/config/SecurityConfig.java
+++ src/main/java/com/account/yomankum/config/SecurityConfig.java
@@ -3,8 +3,13 @@ package com.account.yomankum.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -15,5 +20,31 @@ class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request.requestMatchers("/").hasRole("USER")
+                        .anyRequest().permitAll())
+
+//                .addFilterBefore(new JwtFilter(userDetailsService(), jwtTokenParser, jwtTokenService), UsernamePasswordAuthenticationFilter.class)
+//                .addFilterAfter(new JwtTokenSetFilter(), UsernamePasswordAuthenticationFilter.class)
+                // OAuth2
+//                .addFilterBefore(new CustomOAuth2AuthorizationCodeGrantFilter(clientRegistrationRepository, oAuth2AuthorizedClientRepository, authenticationManager(authenticationConfiguration()), customDefaultOAuth2UserService, snsInfo), OAuth2LoginAuthenticationFilter.class)
+//                .addFilterAfter(new OAuth2JwtTokenFilter(webService, jwtTokenService, jwtTokenParser, memberJoinService, snsInfo, kakaoJwk, googleJwk), OAuth2LoginAuthenticationFilter.class)
+//                .exceptionHandling(exception -> exception.authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+//                        .accessDeniedHandler(new CustomAccessDeniedHandler()))
+
+//                .oauth2Login(oauth -> oauth.loginPage("/loginForm")
+//                        .authorizationEndpoint(end -> end.authorizationRequestResolver(customOAuth2AuthorizationRequestResolver))
+//                        .userInfoEndpoint(userInfo -> userInfo.userService(customDefaultOAuth2UserService))
+//                        .successHandler(new CustomOAuth2SuccessHandler(jwtTokenService, memberJoinService)))
+
+                .logout(logout -> logout.logoutSuccessUrl("/login").permitAll())
+                .build();
     }
 }
