311ce77461d396d0b828f738ef63d115ef486f3f
h0l1da2 <whaleee@naver.com>
2023-11-07 20:07:49 +0900

    feat: 유저디테일관련 클래스 추가
    유저디테일즈, 디테일서비스 클래스 추가

--- src/main/java/com/account/yomankum/config/CustomUserDetails.java
+++ src/main/java/com/account/yomankum/config/CustomUserDetails.java
@@ -1,2 +1,58 @@
-package com.account.yomankum.config;public class CustomUserDetails {
+package com.account.yomankum.config;
+
+import com.account.yomankum.domain.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static java.time.LocalDateTime.*;
+
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoleList(user);
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return now().isBefore(user.getPwdChangeDate().plusMonths(3L));
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.getStopDate() == null ? true : false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getRemoveDate() == null ? true : false;
+    }
+
+    private Collection<GrantedAuthority> getRoleList(User user) {
+        Collection<GrantedAuthority> roleList = new ArrayList<>();
+        roleList.add(() -> user.getRole().getName().name());
+        return roleList;
+    }
 }
--- src/main/java/com/account/yomankum/config/CustomUserDetailsService.java
+++ src/main/java/com/account/yomankum/config/CustomUserDetailsService.java
@@ -1,2 +1,28 @@
-package com.account.yomankum.config;public class EtcConfig {
+package com.account.yomankum.config;
+
+import com.account.yomankum.domain.User;
+import com.account.yomankum.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User findUser = userRepository.findByUsername(username).orElse(null);
+
+        if (findUser == null) {
+            throw new UsernameNotFoundException("유저를 찾을 수 없음");
+        }
+
+        return new CustomUserDetails(findUser);
+    }
+
 }
