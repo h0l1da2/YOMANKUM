651f8e154ecc70b833b27f16628d6092643a720a
h0l1da2 <whaleee@naver.com>
2023-11-07 20:05:39 +0900

    feat: User 회원가입 데이터 추가
    가입할 때 패스워드 변경 날짜도 함께 주가

--- src/main/java/com/account/yomankum/service/impl/UserServiceImpl.java
+++ src/main/java/com/account/yomankum/service/impl/UserServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,6 +44,7 @@ public class UserServiceImpl implements UserService {
                 .username(username)
                 .password(encodePwd)
                 .role(new Role(Name.ROLE_USER))
+                .pwdChangeDate(LocalDateTime.now())
                 .build();
 
         userRepository.save(user);
