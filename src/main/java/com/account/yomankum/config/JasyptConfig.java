26cd6fe21af589ab64d857a9462a20059ec7236e
h0l1da2 <whaleee@naver.com>
2023-11-16 19:17:18 +0900

    feat: jasyptConfig 추가
    암호화 설정 추가

--- src/main/java/com/account/yomankum/config/JasyptConfig.java
+++ src/main/java/com/account/yomankum/config/JasyptConfig.java
@@ -1,2 +1,30 @@
-package com.account.yomankum.config;public class JasyptConfig {
+package com.account.yomankum.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+
+        String key = "3FF8B8CF78B8535AF641B587AFEC3";
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(key); // 암호화할 때 사용하는 키
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256"); // 암호화 알고리즘
+        config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+        config.setPoolSize("1"); // 인스턴스 pool
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64"); //인코딩 방식
+        encryptor.setConfig(config);
+        return encryptor;
+
+    }
 }
