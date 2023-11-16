d80c20494895c21d17412b0d1e7f339ed3cbbbca
h0l1da2 <whaleee@naver.com>
2023-11-16 19:58:17 +0900

    feat: 암호화 알고리즘 수정
    보편적인 것으로 수정

--- src/main/java/com/account/yomankum/config/JasyptConfig.java
+++ src/main/java/com/account/yomankum/config/JasyptConfig.java
@@ -16,12 +16,12 @@ public class JasyptConfig {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword(key); // 암호화할 때 사용하는 키
-        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256"); // 암호화 알고리즘
+        config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
         config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
         config.setPoolSize("1"); // 인스턴스 pool
         config.setProviderName("SunJCE");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
-        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
         config.setStringOutputType("base64"); //인코딩 방식
         encryptor.setConfig(config);
         return encryptor;
