1f5e2e632b9283fad5d6759c000a95f8b868098e
h0l1da2 <whaleee@naver.com>
2023-11-18 17:54:04 +0900

    feat: SNS 로그인, 회원가입 추가
    로그인 , 회원가입 추가
    
    휴일

--- src/main/java/com/account/yomankum/repository/SnsUserRepository.java
+++ src/main/java/com/account/yomankum/repository/SnsUserRepository.java
@@ -1,2 +1,11 @@
-package com.account.yomankum.repository;public interface SnsUserRepository {
+package com.account.yomankum.repository;
+
+import com.account.yomankum.config.oauth.Sns;
+import com.account.yomankum.domain.SnsUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SnsUserRepository extends JpaRepository<SnsUser, Long> {
+    Optional<SnsUser> findByEmailAndUAndUuidKeyAndSns(Sns sns, String email, String uuidKey);
 }
