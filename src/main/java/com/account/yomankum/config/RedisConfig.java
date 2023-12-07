d9bab570ae4d5f5a82977975cb437f316938d756
h0l1da2 <whaleee@naver.com>
2023-12-07 13:37:40 +0900

    feat: redis 설정 추가
    설정 추가

--- src/main/java/com/account/yomankum/config/RedisConfig.java
+++ src/main/java/com/account/yomankum/config/RedisConfig.java
@@ -3,12 +3,14 @@ package com.account.yomankum.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@PropertySource("classpath:application.yml")
 @EnableRedisRepositories
 public class RedisConfig {
 
