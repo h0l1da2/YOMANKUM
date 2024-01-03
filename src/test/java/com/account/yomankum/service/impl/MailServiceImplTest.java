221e53c7714c29cdfcb78d896f0e0ad21e18a5f5
h0l1da2 <whaleee@naver.com>
2024-01-03 16:40:24 +0900

    test: MailService Test
    - Mail Send Test
    
    #8

--- src/test/java/com/account/yomankum/service/impl/MailServiceImplTest.java
+++ src/test/java/com/account/yomankum/service/impl/MailServiceImplTest.java
@@ -2,6 +2,7 @@ package com.account.yomankum.service.impl;
 
 import com.account.yomankum.domain.Mail;
 import com.account.yomankum.service.MailService;
+import com.account.yomankum.util.RedisUtil;
 import jakarta.mail.MessagingException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,8 @@ class MailServiceImplTest {
 
     @Autowired
     private MailService mailService;
+    @Autowired
+    private RedisUtil redisUtil;
 
     @Test
     @DisplayName("회원가입 메일 보내기 성공")
@@ -23,5 +26,18 @@ class MailServiceImplTest {
 
         assertNotNull(code);
         System.out.println("code = " + code);
+    }
+
+    @Test
+    @DisplayName("메일 코드 레디스 저장 완료")
+    void 레디스_코드_저장_성공() throws MessagingException {
+        String mail = "holiday.k1@icloud.com";
+        String code = mailService.mailSend(Mail.JOIN, mail);
+
+        assertNotNull(code);
+
+        String redisCode = redisUtil.getData(mail);
+
+        assertEquals(code, redisCode);
     }
 }
