05490e6aaed6cf491a39847a610c2021fcb79ad0
h0l1da2 <whaleee@naver.com>
2024-01-03 18:53:40 +0900

    feat: add Log
    - add Log and Change Exception
    
    #8

--- src/main/java/com/account/yomankum/service/UserService.java
+++ src/main/java/com/account/yomankum/service/UserService.java
@@ -2,12 +2,10 @@ package com.account.yomankum.service;
 
 import com.account.yomankum.domain.dto.LoginDto;
 import com.account.yomankum.domain.dto.UserSignUpDto;
-import com.account.yomankum.exception.IncorrectLoginException;
-import com.account.yomankum.exception.UserDuplicateException;
 
 import java.util.Map;
 
 public interface UserService {
-    void signUp(UserSignUpDto userSignUpDto) throws UserDuplicateException;
-    Map<String, String> login(LoginDto loginDto) throws UserDuplicateException, IncorrectLoginException;
+    void signUp(UserSignUpDto userSignUpDto) throws IllegalArgumentException;
+    Map<String, String> login(LoginDto loginDto) throws IllegalArgumentException;
 }
