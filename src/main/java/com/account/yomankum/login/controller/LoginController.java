909cb7b953cde0da6ede113c9705b2a1c5217fc3
h0l1da2 <whaleee@naver.com>
2024-01-05 22:38:20 +0900

    feat: Add Springdoc Setting
    - Add @Tag (Title)
    - Add @Operation (Comment)
    
    PR #12 Close

--- src/main/java/com/account/yomankum/login/controller/LoginController.java
+++ src/main/java/com/account/yomankum/login/controller/LoginController.java
@@ -4,6 +4,8 @@ import com.account.yomankum.login.domain.LoginDto;
 import com.account.yomankum.login.service.UserService;
 import com.account.yomankum.web.response.Response;
 import com.account.yomankum.web.response.ResponseCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,16 +18,13 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/login")
+@Tag(name = "LOGIN", description = "로그인 페이지 API 명세서")
 public class LoginController {
 
     private final UserService userService;
 
-    @GetMapping
-    public ResponseEntity<Response> loginMain() {
-        return Response.ok();
-    }
-
     @PostMapping
+    @Operation(summary = "일반 회원 로그인", description = "일반 회원용 로그인")
     public ResponseEntity<Response> login(@RequestBody @Valid LoginDto loginDto) {
 
         Map<String, String> tokens = null;
