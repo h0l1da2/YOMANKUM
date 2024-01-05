439cd4bd42b1ce4809f47e03a92e9f0164819c5c
h0l1da2 <whaleee@naver.com>
2024-01-05 22:30:44 +0900

    feat: Add Springdoc Configuration
    - Add Springdoc Configuration
    
    #12

--- src/main/java/com/account/yomankum/config/SpringdocConfig.java
+++ src/main/java/com/account/yomankum/config/SpringdocConfig.java
@@ -1,2 +1,26 @@
-package com.account.yomankum.config;public class SpringdocConfig {
+package com.account.yomankum.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class SpringdocConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("YOMANKUM API 명세서")
+                .description("태그로 정리하는 편리한 가계부 요만큼의 API 명세서")
+                .version("1.0.0");
+    }
+
 }
