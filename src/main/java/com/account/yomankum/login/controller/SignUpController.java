335975eeca19f0890d96e47bc7081c649e7d6d6d
h0l1da2 <whaleee@naver.com>
2024-01-05 22:38:20 +0900

    feat: Add Springdoc Setting
    - Add @Tag (Title)
    - Add @Operation (Comment)
    
    PR #12 Close

--- src/main/java/com/account/yomankum/login/controller/SignUpController.java
+++ src/main/java/com/account/yomankum/login/controller/SignUpController.java
@@ -9,6 +9,8 @@ import com.account.yomankum.login.service.UserService;
 import com.account.yomankum.web.response.Response;
 import com.account.yomankum.web.response.ResponseCode;
 import com.sun.mail.smtp.SMTPAddressFailedException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,17 +22,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/signUp")
+@Tag(name = "(NORMAL) SIGN UP", description = "일반 회원가입 API 명세서")
 public class SignUpController {
 
     private final UserService userService;
     private final MailService mailService;
 
-    @GetMapping
-    public ResponseEntity<Response> signUpMain() {
-        return Response.ok();
-    }
-
     @PostMapping
+    @Operation(summary = "일반 회원 가입", description = "일반 회원 가입 완료")
     public ResponseEntity<Response> signUp(@RequestBody @Valid UserSignUpDto userSignUpDto) {
 
         // 회원가입
@@ -40,6 +39,7 @@ public class SignUpController {
     }
 
     @PostMapping("/email/send")
+    @Operation(summary = "인증 메일 보내기", description = "회원가입용 인증 메일 보내기")
     public ResponseEntity<Response> sendEmailCode(@RequestBody @Valid EmailDto emailDto) {
 
         String code = null;
@@ -68,6 +68,7 @@ public class SignUpController {
     }
 
     @PostMapping("/email/check")
+    @Operation(summary = "메일 인증 코드 체크", description = "메일 인증 코드 체크")
     public ResponseEntity<Response> checkEmailCode(@RequestBody @Valid EmailCodeDto emailCodeDto) {
 
         boolean isSuccessMailCode = mailService.verifyEmailCode(emailCodeDto.email(), emailCodeDto.code());
