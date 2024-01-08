339afc4efc23d2931f7172ed8d4f4d8783c337c9
h0l1da2 <whaleee@naver.com>
2024-01-08 23:36:33 +0900

    feat: Add Sns Login Service
    - Add Sns Login Service
    - Return SnsUser Entity
    
    #16

--- src/main/java/com/account/yomankum/security/service/SnsUserService.java
+++ src/main/java/com/account/yomankum/security/service/SnsUserService.java
@@ -1,11 +1,11 @@
-package com.account.yomankum.login.service;
+package com.account.yomankum.security.service;
 
 import com.account.yomankum.security.oauth.Sns;
 import com.account.yomankum.domain.SnsUser;
 
 public interface SnsUserService {
 
-    SnsUser loginCheck(Sns sns, String uuidKey, String email);
+    SnsUser login(Sns sns, String uuidKey);
     SnsUser signUp(Sns sns, String email, String uuidKey);
 
 }
--- src/main/java/com/account/yomankum/security/service/SnsUserServiceImpl.java
+++ src/main/java/com/account/yomankum/security/service/SnsUserServiceImpl.java
@@ -1,37 +1,35 @@
-package com.account.yomankum.login.service;
+package com.account.yomankum.security.service;
 
-import com.account.yomankum.security.oauth.Sns;
-import com.account.yomankum.domain.enums.Name;
 import com.account.yomankum.domain.Role;
 import com.account.yomankum.domain.SnsUser;
+import com.account.yomankum.domain.enums.Name;
 import com.account.yomankum.repository.SnsUserRepository;
+import com.account.yomankum.security.oauth.Sns;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import static java.time.LocalDateTime.*;
+import static java.time.LocalDateTime.now;
 
 @Service
 @RequiredArgsConstructor
 public class SnsUserServiceImpl implements SnsUserService {
 
     private final SnsUserRepository snsUserRepository;
-
     @Override
-    public SnsUser loginCheck(Sns sns, String email, String uuidKey) {
-        return snsUserRepository.findByEmailAndUuidKeyAndSns(sns, email, uuidKey).orElse(null);
+    public SnsUser login(Sns sns, String uuidKey) {
+        return snsUserRepository.findByUuidKeyAndSns(sns, uuidKey).orElse(null);
     }
 
     @Override
     public SnsUser signUp(Sns sns, String email, String uuidKey) {
         SnsUser snsUser = SnsUser.builder()
                 .uuidKey(uuidKey)
-                .email(email)
                 .joinDate(now())
+                .email(email)
                 .pwdChangeDate(now())
                 .sns(sns)
                 .role(new Role(Name.ROLE_USER))
                 .build();
 
-        return snsUserRepository.save(snsUser);
-    }
+        return snsUserRepository.save(snsUser);    }
 }
