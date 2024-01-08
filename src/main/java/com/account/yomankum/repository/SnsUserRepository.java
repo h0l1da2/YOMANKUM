2e229b65f14f588cee18170069bf326ab91800b3
h0l1da2 <whaleee@naver.com>
2024-01-08 23:37:14 +0900

    feat: add findByUuidKeyAndSns
    - Add FindByUuidKeyAndSns
    
    #16

--- src/main/java/com/account/yomankum/repository/SnsUserRepository.java
+++ src/main/java/com/account/yomankum/repository/SnsUserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SnsUserRepository extends JpaRepository<SnsUser, Long> {
-    Optional<SnsUser> findByEmailAndUuidKeyAndSns(Sns sns, String email, String uuidKey);
+    Optional<SnsUser> findByUuidKeyAndSns(Sns sns, String uuidKey);
 }
