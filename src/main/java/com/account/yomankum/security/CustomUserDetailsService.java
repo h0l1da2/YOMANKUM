6fd83a5b1ff76eb5035a233c12c297902508f3a1
hyungjin <hyungzin0309@naver.com>
2024-01-19 22:17:30 +0900

    test : 실패 test disabled 처리

--- src/main/java/com/account/yomankum/security/CustomUserDetailsService.java
+++ src/main/java/com/account/yomankum/security/CustomUserDetailsService.java
@@ -18,7 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         long id = Long.parseLong(username);
-        User findUser = userRepository.findById(id).orElseThrow(()->new RuntimeException(ResponseCode.EMAIL000.toString()));
+        User findUser = userRepository.findById(id).orElseThrow(()->new RuntimeException(ResponseCode.USER_NOT_FOUND.toString()));
         return new CustomUserDetails(findUser);
     }
 
