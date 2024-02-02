3bcc7168291a606c06bccb71b7c6edf146093b4b
h0l1da2 <whaleee@naver.com>
2024-02-02 20:52:55 +0900

    refactor: @Valid 핸들러 수정
    - 필요 없는 주석 삭제
    
    24

--- src/main/java/com/account/yomankum/web/handler/GlobalExceptionHandler.java
+++ src/main/java/com/account/yomankum/web/handler/GlobalExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.account.yomankum.web.handler;
 
-import com.account.yomankum.web.response.Response;
-import com.account.yomankum.web.response.ResponseCode;
 import com.nimbusds.jose.shaded.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,18 +14,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // TODO Valid 에러 핸들링 없이 잡는 법 ?
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
-    public ResponseEntity<Response> invalidRequestHandler(MethodArgumentNotValidException e) {
-        log.error("@Valid 에러 : {}", e.getMessage());
-        e.printStackTrace();
-
+    public JsonObject invalidRequestHandler(MethodArgumentNotValidException e) {
         JsonObject invalid = new JsonObject();
         for (FieldError error : e.getFieldErrors()) {
-            invalid.addProperty(error.getField(), error.getDefaultMessage());
+            invalid.addProperty(
+                    error.getField(), error.getDefaultMessage()
+            );
         }
-
-        return Response.badRequest(ResponseCode.NOT_VALID, invalid);
+        return invalid;
     }}
