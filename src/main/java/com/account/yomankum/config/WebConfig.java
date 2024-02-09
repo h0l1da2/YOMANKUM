fd9ff86165ef744373ecfa3dfbeb07ee8cacc787
hyungjin <hyungzin0309@naver.com>
2024-02-09 19:44:27 +0900

    config : cors 설정

---
+++ src/main/java/com/account/yomankum/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.account.yomankum.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false) // 와일드카드 사용 시 false로 설정
+                .maxAge(3600);
+    }
+
+}
