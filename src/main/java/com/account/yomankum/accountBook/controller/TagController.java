ed153790113c28dbf888e3b8a4ed962cb80571df
hyungjin <hyungzin0309@naver.com>
2024-02-13 16:51:12 +0900

    feat : 대분류 태그 찾는 api 추가

---
+++ src/main/java/com/account/yomankum/accountBook/controller/TagController.java
@@ -0,0 +1,29 @@
+package com.account.yomankum.accountBook.controller;
+
+import com.account.yomankum.accountBook.service.TagFinder;
+import io.swagger.v3.oas.annotations.Operation;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/tag")
+public class TagController {
+
+    private final TagFinder tagFinder;
+
+    @GetMapping("/{accountBookId}")
+    @Operation(summary = "특정 가계부에서 사용되는 모든 대분류 태그", description = "기간조건을 이용하여 특정 기간에 사용된 대분류 태그만 검색할 수 있다.")
+    public List<String> getMajorTags(@PathVariable Long accountBookId,
+            @RequestParam LocalDate from,
+            @RequestParam LocalDate to){
+        return tagFinder.findMajorTagsByAccountBook(accountBookId, from, to);
+    }
+
+}
