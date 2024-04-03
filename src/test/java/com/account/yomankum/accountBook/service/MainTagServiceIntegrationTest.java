6316b610f98b4d107b16d02dd4ff8deade3c3ed9
hyungjin <hyungzin0309@naver.com>
2024-04-03 18:58:02 +0900

    test : main tag service integration test 추가

---
+++ src/test/java/com/account/yomankum/accountBook/service/MainTagServiceIntegrationTest.java
@@ -0,0 +1,81 @@
+package com.account.yomankum.accountBook.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.account.yomankum.accountBook.domain.AccountBook;
+import com.account.yomankum.accountBook.domain.AccountBookRepository;
+import com.account.yomankum.accountBook.domain.tag.Color;
+import com.account.yomankum.accountBook.domain.tag.MainTagRepository;
+import com.account.yomankum.accountBook.domain.tag.Tag;
+import com.account.yomankum.accountBook.dto.request.MainTagRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+@WithMockUser(username = "1")
+class MainTagServiceIntegrationTest {
+
+    @Autowired private MainTagService mainTagService;
+    @Autowired private MainTagRepository mainTagRepository;
+    @Autowired private AccountBookRepository accountBookRepository;
+    private AccountBook accountBook;
+    private Tag tag;
+
+    @BeforeEach
+    void setup(){
+        accountBook = AccountBook.builder()
+                .name("new account book")
+                .build();
+        accountBookRepository.save(accountBook);
+        tag = new Tag(null, "main tag1", accountBook, new Color());
+        mainTagRepository.save(tag);
+        accountBook.getMainTags().add(tag);
+    }
+
+    @Test
+    @DisplayName("대분류 태그 생성.")
+    public void create_mainTag(){
+        int previousTagSize = accountBook.getMainTags().size();
+        MainTagRequest createRequest = new MainTagRequest("new tag");
+        Tag createdTag = mainTagService.create(accountBook.getId(), createRequest);
+        int currentTagSize = accountBook.getMainTags().size();
+
+        assertEquals(createdTag, mainTagRepository.findById(createdTag.getId()).orElse(null));
+        assertNotEquals(previousTagSize, currentTagSize);
+        assertTrue(accountBook.getMainTags().contains(createdTag));
+    }
+
+    @Test
+    @DisplayName("대분류 태그 수정")
+    public void update_mainTag(){
+        String newName = "new name";
+        mainTagService.update(tag.getId(), new MainTagRequest("new name"));
+
+        assertEquals(newName, mainTagRepository.findById(tag.getId()).orElse(new Tag()).getName());
+    }
+
+    @Test
+    @DisplayName("대분류 태그 삭제.")
+    public void delete_mainTag(){
+        int previousTagSize = accountBook.getMainTags().size();
+        mainTagService.delete(tag.getId());
+        int currentTagSize = accountBook.getMainTags().size();
+
+        assertNotEquals(previousTagSize, currentTagSize);
+        assertFalse(accountBook.getMainTags().contains(tag));
+        assertTrue(mainTagRepository.findById(tag.getId()).isEmpty());
+    }
+
+}
