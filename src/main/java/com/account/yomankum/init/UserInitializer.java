78fe72242e1ffdb7310d3ddb66766e9e03f399f1
hyungjin <hyungzin0309@naver.com>
2024-04-11 17:01:57 +0900

    feat : 유저 초기화 변경

--- src/main/java/com/account/yomankum/init/UserInitializer.java
+++ src/main/java/com/account/yomankum/init/UserInitializer.java
@@ -1,17 +1,25 @@
 package com.account.yomankum.init;
 
+import com.account.yomankum.user.domain.Role;
+import com.account.yomankum.user.domain.User;
+import com.account.yomankum.user.domain.UserType;
+import com.account.yomankum.user.domain.type.RoleName;
 import com.account.yomankum.user.dto.UserDto.UserSignUpDto;
+import com.account.yomankum.user.repository.UserRepository;
 import com.account.yomankum.user.service.UserService;
 import jakarta.annotation.PostConstruct;
+import java.time.Instant;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UserInitializer {
 
-    private final UserService userService;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
     private static final String COMMON_PWD = "Admin123!";
 
     @PostConstruct
@@ -22,8 +30,19 @@ public class UserInitializer {
         save("whaleee@naver.com");
     }
 
-    private void save(String id){
-        userService.signUp(new UserSignUpDto(id, COMMON_PWD));
+    private void save(String email){
+        userRepository.save(newUser(email));
+    }
+
+    private User newUser(String email){
+        return User.builder()
+                .role(new Role(RoleName.ROLE_USER))
+                .userType(UserType.USER)
+                .email(email)
+                .userType(UserType.ADMIN)
+                .password(passwordEncoder.encode(COMMON_PWD))
+                .pwdChangeDatetime(Instant.now())
+                .build();
     }
 
 
