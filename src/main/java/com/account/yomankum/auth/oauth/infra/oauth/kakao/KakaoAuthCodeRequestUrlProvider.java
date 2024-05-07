3f46efa5bde3480c200beaf8580bc7aaac4b09db
hyungjin <hyungzin0309@naver.com>
2024-05-07 16:19:30 +0900

    feat : oauth 추가

---
+++ src/main/java/com/account/yomankum/auth/oauth/infra/oauth/kakao/KakaoAuthCodeRequestUrlProvider.java
@@ -0,0 +1,32 @@
+package com.account.yomankum.auth.oauth.infra.oauth.kakao;
+
+import com.account.yomankum.auth.oauth.domain.oauthCodeRequest.AuthCodeRequestUrlProvider;
+import com.account.yomankum.auth.oauth.infra.oauth.kakao.config.KakaoAuthConfig;
+import com.account.yomankum.auth.oauth.infra.oauth.kakao.config.KakaoProviderConfig;
+import com.account.yomankum.user.domain.AuthType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@RequiredArgsConstructor
+public class KakaoAuthCodeRequestUrlProvider implements AuthCodeRequestUrlProvider {
+
+    private final KakaoProviderConfig providerConfig;
+    private final KakaoAuthConfig authConfig;
+
+    @Override
+    public AuthType support() {
+        return AuthType.KAKAO;
+    }
+
+    @Override
+    public String getUrl() {
+        return UriComponentsBuilder
+                .fromUriString(providerConfig.getAuthorizationUri())
+                .queryParam("response_type", "code")
+                .queryParam("client_id", authConfig.getClientId())
+                .queryParam("redirect_uri", authConfig.getRedirectUri())
+                .toUriString();
+    }
+}
---
+++ src/main/java/com/account/yomankum/auth/oauth/infra/oauth/kakao/config/KakaoAuthConfig.java
@@ -0,0 +1,19 @@
+package com.account.yomankum.auth.oauth.infra.oauth.kakao.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "oauth.kakao")
+public class KakaoAuthConfig {
+
+    private String clientId;
+    private String clientSecret;
+    private String redirectUri;
+    private String authorizationGrantType;
+    private String clientAuthenticationMethod;
+
+    private String authorizationUri; // code요청 url
+}
