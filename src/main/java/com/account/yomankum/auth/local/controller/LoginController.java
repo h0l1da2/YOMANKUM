ecac0bd6935a1f04d2957018cca2e86db3296f14
hyungjin <hyungzin0309@naver.com>
2024-05-07 16:19:58 +0900

    feat : oauth 추가

--- src/main/java/com/account/yomankum/auth/local/controller/LoginController.java
+++ src/main/java/com/account/yomankum/auth/local/controller/LoginController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.account.yomankum.user.dto.UserDto.UserLoginDto;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
---
+++ src/main/java/com/account/yomankum/auth/oauth/infra/oauth/kakao/response/KakaoUserResponse.java
@@ -0,0 +1,37 @@
+package com.account.yomankum.auth.oauth.infra.oauth.kakao.response;
+
+import com.account.yomankum.user.domain.AuthInfo;
+import com.account.yomankum.user.domain.User;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class KakaoUserResponse {
+
+    private Long id;
+    private KakaoAccount kakaoAccount;
+
+    public User toEntity(){
+        AuthInfo authInfo = new AuthInfo();
+        return User.builder()
+                .authInfo(authInfo)
+                .nickname(kakaoAccount.profile.nickname)
+                .picture(kakaoAccount.profile.profileImageUrl)
+                .build();
+    }
+
+    @Data
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public class KakaoAccount{
+        Profile profile;
+
+        @Data
+        @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+        public class Profile{
+            String nickname;
+            String profileImageUrl;
+        }
+    }
+}
