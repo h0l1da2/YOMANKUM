7d50c03183a2899efb62713afd166341d3b5ea55
hyungjin <hyungzin0309@naver.com>
2024-05-20 19:24:21 +0900

    test : token controller test 추가

--- src/test/java/com/account/yomankum/common/ControllerTest.java
+++ src/test/java/com/account/yomankum/common/ControllerTest.java
@@ -1,11 +1,11 @@
 package com.account.yomankum.common;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
 import com.account.yomankum.auth.common.AuthArgumentResolver;
 import com.account.yomankum.auth.jwt.service.TokenService;
 import com.account.yomankum.config.RestDocsConfiguration;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
