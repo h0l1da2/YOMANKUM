7f6a812182724ee4e8386d3c17f14d1073b46816
hyungjin <hyungzin0309@naver.com>
2024-05-21 08:30:26 +0900

    refactor : 미사용 import 삭제

--- src/main/java/com/account/yomankum/auth/jwt/controller/TokenController.java
+++ src/main/java/com/account/yomankum/auth/jwt/controller/TokenController.java
@@ -4,10 +4,8 @@ import com.account.yomankum.auth.jwt.service.RefreshTokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
