0407f689ee47d5bac9e9ff78949e638d1e8877e2
h0l1da2 <whaleee@naver.com>
2024-07-31 21:51:04 +0900

    feat(kafka): input 용 코드 추가
    쓰기 input 용 코드 추가
    
    85

--- src/main/java/com/account/yomankum/kafka/KafkaService.java
+++ src/main/java/com/account/yomankum/kafka/KafkaService.java
@@ -5,7 +5,9 @@ import com.account.yomankum.auth.common.LoginUser;
 import com.account.yomankum.common.exception.BadRequestException;
 import com.account.yomankum.common.exception.Exception;
 import com.account.yomankum.kafka.dto.AccountBookCreateNotice;
+import com.account.yomankum.kafka.dto.AccountBookInputNotice;
 import com.account.yomankum.kafka.dto.AccountBookUpdateNotice;
+import com.account.yomankum.socket.common.CustomWebSocketHandler;
 import com.account.yomankum.user.domain.User;
 import com.account.yomankum.user.service.UserFinder;
 import lombok.RequiredArgsConstructor;
@@ -19,18 +21,18 @@ import org.springframework.stereotype.Component;
 public class KafkaService {
 
     private final UserFinder userFinder;
+    private final CustomWebSocketHandler customWebSocketHandler;
+
+    @KafkaListener(topics = "input", groupId = "accountBook")
+    public void inputAccountBookNotification(AccountBookInputNotice notice) {
+        customWebSocketHandler.sendAccountBookInputMessage(notice);
+        log.info("[Kafka] input 메시지 수신 - accountBookId : {}", notice.accountBookId());
+    }
 
     @KafkaListener(topics = "create", groupId = "accountBook")
-    public void createAccountBookNotification(@Auth LoginUser loginUser, AccountBookCreateNotice notice) {
-        User user = userFinder.findById(loginUser.getUserId())
-                .orElseThrow(() -> new BadRequestException(Exception.USER_NOT_FOUND));
-
-        boolean isExist = user.isUsersAccountBook(notice.accountBookId());
-
-        if (isExist) {
-            // 알림 추가 -> Http ?
-            log.info("[Kafka] create 메시지 수신 - accountBookId : {}", notice.accountBookId());
-        }
+    public void createAccountBookNotification(AccountBookCreateNotice notice) {
+        // 알림 추가 -> Http ?
+        log.info("[Kafka] create 메시지 수신 - accountBookId : {}", notice.accountBookId());
     }
 
     @KafkaListener(topics = "update", groupId = "accountBook")
